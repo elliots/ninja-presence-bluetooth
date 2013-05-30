4d27230ea4f445f4cfe94977b8365c1c962e7cbc
Elliot Shepherd <elliot@jarofworms.com>
2013-05-30 23:27:48 +1000

    Initial (COMPLETELY UNTESTED) version

---
+++ index.js
@@ -0,0 +1,26 @@
+var Presence = require('ninja-presence-base');
+
+module.exports = Presence;
+
+Presence.prototype.G = 'bluetooth';
+
+Presence.prototype.init = function() {
+  var self = this;
+  this._bt = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+
+  this._bt.on('found', function(address, name) {
+    self.see({
+      name: name,
+      id: address
+    });
+    self._app.log.info("Found bluetooth device", name, address);
+  });
+
+  bt.on('finished', function() {
+    self.scanComplete();
+  });
+};
+
+Presence.prototype.scan = function() {
+  this._bt.inquire();
+};
